ef2636e2b2feeab96d527e1a29441d064fa5284f
Tahera Shorna <taherashorna@taheras-mbp.home>
2020-02-23 02:54:12 +1100

    Section 4 completed

---
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  model: any ={};
+  @Output() cancelRegister = new EventEmitter();
+
+  constructor(private authservice: AuthService) { }
+
+  ngOnInit() {
+  }
+  register(){
+    this.authservice.register(this.model).subscribe(() => {
+      console.log('registration successful!');
+    }, error =>{
+      console.log(error);
+    });
+  }
+
+  cancel(){
+    this.cancelRegister.emit(false);
+    console.log('cancelled');
+  }
+
+
+
+}
